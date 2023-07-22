87c09317899d209e64bd47f0b04be67701e5df61
saberali1789 <78509035+saberali1789@users.noreply.github.com>
2023-07-23 02:46:37 +0300

    projects

---
+++ app.js
@@ -0,0 +1,29 @@
+
+
+
+const projects = [
+    {
+        title:'Project1',
+        discription: 'Project',
+        img:'./img/car.png',
+    },
+    {
+        title:'Project1',
+        discription: 'Project',
+        img:'./img/car.png',
+    },
+    {
+        title:'Project1',
+        discription: 'Project',
+        img:'./img/car.png',
+    },
+    {
+        title:'Project1',
+        discription: 'Project',
+        img:'./img/car.png',
+    },
+]
+
+projects.forEach(project=>{
+    
+})
